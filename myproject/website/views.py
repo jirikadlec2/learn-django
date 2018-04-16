eea77244540ef485037b0455fb0612acda8aa705
jirikadlec2 <jirikadlec2@gmail.com>
2018-04-16 23:38:45 +0200

    using template for Boards

--- myproject/website/views.py
+++ myproject/website/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Board
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Ahoj světe!")
+    boards = Board.objects.all()
+
+    return render(request, 'home.html', {'boards':boards})
+    response_html = '<br>'.join(boards_names)
+    return HttpResponse(response_html)
